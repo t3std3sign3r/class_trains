f6056dffecdeefe2026b1cd62d9aaf00b7a54b3c
t3std3sign3r <tai.kharchenko@yandex.ru>
2024-02-22 13:56:50 +0300

    plus func changes

--- first/temp.py
+++ first/temp.py
@@ -2,5 +2,10 @@ def hello(greet='git'):
     return f'Hello, {greet}'
 
 
+def changes():
+    return f'This changes for second commit'
+
+
 if __name__ == '__main__':
     print(hello('main'))
+    print(changes())
